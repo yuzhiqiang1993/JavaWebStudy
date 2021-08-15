9ee90297801d211ccd7ac5af999d9cc8455109d8
XeonYu <zhiqiang.yu.xeon@gmail.com>
2021-08-15 17:52:36 +0800

    add resultMap

--- MyBatis/MyBatisModule/src/main/java/com/xeon/pojo/User.java
+++ MyBatis/MyBatisModule/src/main/java/com/xeon/pojo/User.java
@@ -5,8 +5,8 @@ import lombok.Data;
 @Data
 public class User {
 
-    private String org_id = "";
-    private String default_company_code = "";
+    private String orgId = "";
+    private String defaultCompanyCode = "";
     private String name = "";
     private String gid = "";
     private Integer id;
@@ -16,16 +16,16 @@ public class User {
     }
 
     public User(String org_id, String default_company_code, String name, String gid, Integer id) {
-        this.org_id = org_id;
-        this.default_company_code = default_company_code;
+        this.orgId = org_id;
+        this.defaultCompanyCode = default_company_code;
         this.name = name;
         this.gid = gid;
         this.id = id;
     }
 
     public User(String ord_id, String default_company_code, String name, String gid) {
-        this.org_id = ord_id;
-        this.default_company_code = default_company_code;
+        this.orgId = ord_id;
+        this.defaultCompanyCode = default_company_code;
         this.name = name;
         this.gid = gid;
     }
