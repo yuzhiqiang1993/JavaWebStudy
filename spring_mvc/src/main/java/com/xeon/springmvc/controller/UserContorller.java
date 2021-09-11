ee6140cc4a1530cc7c123220c57f0120b0b2081e
XeonYu <zhiqiang.yu.xeon@gmail.com>
2021-09-11 14:00:19 +0800

    传参示例

--- spring_mvc/src/main/java/com/xeon/springmvc/controller/UserContorller.java
+++ spring_mvc/src/main/java/com/xeon/springmvc/controller/UserContorller.java
@@ -4,26 +4,60 @@ package com.xeon.springmvc.controller;
 import com.google.gson.Gson;
 import com.xeon.springmvc.data.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpServletResponse;
-
 @Controller
 public class UserContorller {
 
-    @RequestMapping(value = "user", method = RequestMethod.GET)
+    @RequestMapping(value = "user", method = {RequestMethod.GET, RequestMethod.POST})
     @ResponseBody
-    public String getUser(HttpServletResponse response) {
-
-        response.setContentType("application/json;charset=UTF-8");
+    public String getUser(String id) {
+        /*参数可以直接拿到  get和post 表单形式的都支持*/
         System.out.println("user 执行了");
 
+        System.out.println("id = " + id);
         final User user = new User();
 
         user.setAge(28);
         user.setName("喻志强");
+        return new Gson().toJson(user);
+
+    }
+
+
+    /**
+     * 这种会把 表单 传的参数自动封成user对象
+     *
+     * @param user
+     * @return
+     */
+    @RequestMapping(value = "user1", method = {RequestMethod.GET, RequestMethod.POST})
+    @ResponseBody
+    public String getUser1(User user) {
+
+
+        System.out.println("user = " + user);
+
+        return new Gson().toJson(user);
+
+    }
+
+    /**
+     * 这种会把 表单 传的参数自动封成user对象
+     *
+     * @param user
+     * @return
+     */
+    @RequestMapping(value = "userByJson", method = {RequestMethod.GET, RequestMethod.POST})
+    @ResponseBody
+    public String getUserByJson(@RequestBody User user) {
+
+
+        System.out.println("getUserByJson user = " + user);
+
         return new Gson().toJson(user);
 
     }
--- spring_mvc/src/main/java/com/xeon/springmvc/data/User.java
+++ spring_mvc/src/main/java/com/xeon/springmvc/data/User.java
@@ -1,12 +1,20 @@
 package com.xeon.springmvc.data;
 
 
+import jdk.nashorn.internal.objects.annotations.Constructor;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.stereotype.Component;
 
 @Data
+@Component
+@AllArgsConstructor
 public class User {
 
     private String name;
     private Integer id;
     private Integer age;
+
+    public User() {
+    }
 }
