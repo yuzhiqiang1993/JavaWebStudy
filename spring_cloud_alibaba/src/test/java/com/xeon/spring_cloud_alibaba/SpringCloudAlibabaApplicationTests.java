cc09d05088920ffe5a38d594e5d7f7de3238448d
XeonYu <zhiqiang.yu.xeon@gmail.com>
2021-10-03 20:56:46 +0800

    新增spring cloud alibaba工程

---
+++ spring_cloud_alibaba/src/test/java/com/xeon/spring_cloud_alibaba/SpringCloudAlibabaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xeon.spring_cloud_alibaba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudAlibabaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
