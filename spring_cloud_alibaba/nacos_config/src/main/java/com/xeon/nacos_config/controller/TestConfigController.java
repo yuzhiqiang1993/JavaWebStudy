53a613a8348a85a17c24439aaf984f0641a03bc2
XeonYu <zhiqiang.yu.xeon@gmail.com>
2021-10-05 19:37:04 +0800

    新增读取nacos远程配置示例

---
+++ spring_cloud_alibaba/nacos_config/src/main/java/com/xeon/nacos_config/controller/TestConfigController.java
@@ -0,0 +1,29 @@
+package com.xeon.nacos_config.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : yuzhiqiang (zhiqiang.yu.xeon@gmail.com)
+ * @description: 测试远程配置
+ * @date : 2021/10/5
+ * @time : 18:25
+ */
+
+@RestController
+public class TestConfigController {
+
+    @Value("${remote.config}")
+    private String remoteConfigVal;
+
+
+    @GetMapping("/config")
+    public String getRemoteConfigVal() {
+
+        return this.remoteConfigVal;
+    }
+
+
+}
+
