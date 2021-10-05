fd3032ec1f6ae8b9bd5b2f5ee20d960c6594ccd4
XeonYu <zhiqiang.yu.xeon@gmail.com>
2021-10-05 19:38:55 +0800

    新增动态读取nacos配置

--- spring_cloud_alibaba/nacos_config/src/main/java/com/xeon/nacos_config/controller/TestConfigController.java
+++ spring_cloud_alibaba/nacos_config/src/main/java/com/xeon/nacos_config/controller/TestConfigController.java
@@ -1,6 +1,7 @@
 package com.xeon.nacos_config.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
+@RefreshScope
 public class TestConfigController {
 
     @Value("${remote.config}")
