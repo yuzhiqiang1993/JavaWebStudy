c43e4dfad6757072cb6e15a210e470fa6367a505
XeonYu <zhiqiang.yu.xeon@gmail.com>
2021-10-06 13:40:12 +0800

    udpate 更正类名

--- spring_cloud_alibaba/gateway/src/main/java/com/xeon/gateway/GatewayApplication.java
+++ spring_cloud_alibaba/gateway/src/main/java/com/xeon/gateway/GatewayApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GetwayApplication {
+public class GatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GetwayApplication.class, args);
+        SpringApplication.run(GatewayApplication.class, args);
     }
 
 }
