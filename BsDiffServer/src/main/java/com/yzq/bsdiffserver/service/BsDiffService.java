b3878e60c0145a5afa358bc4ea3d541b902e9eca
喻志强 <yuzhiqiang01@shizhuang-inc.com>
2022-01-13 14:05:04 +0800

    update 完善测试代码

--- BsDiffServer/src/main/java/com/yzq/bsdiffserver/service/BsDiffService.java
+++ BsDiffServer/src/main/java/com/yzq/bsdiffserver/service/BsDiffService.java
@@ -7,10 +7,10 @@ import java.io.File;
 
 
 /**
- * @description: 测试类
  * @author : yuzhiqiang (zhiqiang.yu.xeon@gmail.com)
- * @date   : 2021/12/18
- * @time   : 14:55
+ * @description: 测试类
+ * @date : 2021/12/18
+ * @time : 14:55
  */
 public class BsDiffService {
 
@@ -24,13 +24,19 @@ public class BsDiffService {
             String filePath = projectPath + File.separator + "tmp" + File.separator + "bsdiffdemo" + File.separator + "file" + File.separator;
             /*旧文件路径*/
 
-            String oldFilePath = filePath + "old.txt";
+
+//            String testName = "mall_boxs_animations";
+//            String testName = "libeffect";
+            String testName = "app_release_du";
+            String end = ".apk";
+
+            String oldFilePath = filePath + testName + "_old" + end;
             /*新文件*/
-            String newFilePath = filePath + "new.txt";
+            String newFilePath = filePath + testName + "_new" + end;
             /*补丁文件*/
-            String patchFilePath = filePath + "patch.txt";
+            String patchFilePath = filePath + testName + "_patch" + end;
             /*合并后的文件*/
-            String combineFilePath = filePath + "combine.txt";
+            String combineFilePath = filePath + testName + "_combine" + end;
 
             System.out.println("oldFilePath = " + oldFilePath);
             System.out.println("newFilePath = " + newFilePath);
@@ -38,12 +44,17 @@ public class BsDiffService {
             System.out.println("combineFilePath = " + combineFilePath);
 
             /*生成补丁文件*/
+            final long timeMillis = System.currentTimeMillis();
+            System.out.println("正在生成补丁文件，请稍后，具体时跟文件大小有关");
             final int diffResult = BsDiffUtil.fileDiff(newFilePath, oldFilePath, patchFilePath);
-            System.out.println("diffResult = " + diffResult);
+            System.out.println("补丁文件生成结束 code = " + diffResult + "   耗时:" + (System.currentTimeMillis() - timeMillis));
 
+
+            final long millis = System.currentTimeMillis();
+            System.out.println("正在合并补丁文件，请稍后，具体时跟文件大小有关");
             /*合并补丁文件*/
             final int patchResult = BsDiffUtil.filePatch(oldFilePath, patchFilePath, combineFilePath);
-            System.out.println("patchResult = " + patchResult);
+            System.out.println("合并文件结束 code = " + patchResult + "耗时：" + (System.currentTimeMillis() - millis));
 
 
         } catch (Exception e) {
