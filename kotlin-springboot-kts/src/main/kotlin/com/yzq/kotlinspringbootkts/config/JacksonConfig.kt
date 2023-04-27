8c7075e383e7fb4400c2a4963ed9328841465f62
yuzhiqiang <zhiqiang.yu.xeon@gmail.com>
2023-04-27 17:18:32 +0800

    update code review

--- kotlin-springboot-kts/src/main/kotlin/com/yzq/kotlinspringbootkts/config/JacksonConfig.kt
+++ kotlin-springboot-kts/src/main/kotlin/com/yzq/kotlinspringbootkts/config/JacksonConfig.kt
@@ -1,12 +1,10 @@
 package com.yzq.kotlinspringbootkts.config
 
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Primary
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
